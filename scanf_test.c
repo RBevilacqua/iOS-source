96f05af39d7e9d04ea024bf98bafe1230569c3d5
atbaines <eshonbig@umich.edu>
2015-03-01 15:44:23 -0600

    test
    test upload

---
+++ scanf_test.c
@@ -0,0 +1,24 @@
+//
+//  scanf_test.c
+//  
+//
+//  Created by Alan Baines on 2/21/15.
+//
+//
+
+#include <stdio.h>
+
+int main(void){
+    int age;
+    char *name = NULL;
+    
+    if(name == 0){
+        printf("Null\n");
+    }
+    
+//    printf("enter name, followed by age\n");
+//    scanf("%s %d",name,&age);
+//    printf("name: %s, age: %d\n",name, age);
+    
+    return 0;
+}
